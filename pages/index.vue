fcb87aa091398f4afba2d48069a7f691759da631
cavadosi <jpeque.moab@gmail.com>
2024-01-11 12:24:21 +0100

    add layouts and components directory, and install tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-slate-900">
+        <div class="flex flex-col justify-center font-mono  text-white">
+            <nav class="flex items-center justify-start bg-slate-900/80 p-4 mb-24">
+                <button href="/index.html"  class="rounded-lg border border-1 border-transparent transform hover:border-cyan-400 hover: p-1">Home</button>
+            </nav>
+            <div class="flex flex-col items-center justify-center h-lg  mb-36 ">
+                <h1 class="text-7xl m-4">Juego JS</h1>
+                <h2 class="text-sm p-6">hecho por Joan Garcia</h2>
+            </div>
+        </div>
+    </div>
+</template> 
